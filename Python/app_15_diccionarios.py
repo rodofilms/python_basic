7999debf5b0eaa953b145adbe68ff384f9f1e02a
rodofilms <rodartejr.rodolfo@hotmail.com>
2020-05-08 22:30:56 -0500

    Add files via upload

---
+++ Python/app_15_diccionarios.py
@@ -0,0 +1,41 @@
+#limpiar pantalla antes de comenzar
+import os
+#comando para limpiar pantalla
+os.system ("cls") 
+
+'''
+    En esta seccion veremos como utilizar diccionarios en python, ya que son listas comunes pero con un indicador visible
+    en cada elemento, para ubicar rapidamente los elementos de la lista.
+'''
+# Vamos a declarar un lista comun para notar la diferencia
+lista_comun_calificaciones = [10,9,8,5]
+print(str(lista_comun_calificaciones)+"\n")
+# Vamos a declarar una lista con diccionario
+lista_diccionario_calificaciones = {"Rodolfo":10,"Kevin":9,"Adrian":8,"Nike":5}
+print(str(lista_diccionario_calificaciones)+"\n")
+# En esta forma podemos visualizar cualquier elemento de la lista, ya que solo tenemos que poner su indicador
+# para que podamos visualizarlo. Ejemplo, veremos la calificacion de Kevin 
+print(str(lista_diccionario_calificaciones["Kevin"])+"\n")
+# Ahora vamos a cambiar la calificacion de Kevin por un 7
+lista_diccionario_calificaciones["Kevin"] = 7
+print(str(lista_diccionario_calificaciones)+"\n")
+
+'''
+    Al igual que las listas, los diccionarios tienen un metodo para ser creados, el cual
+    se llama "dict()"
+'''
+
+lista_calificaciones_nueva = dict([["Kevin",7],["Maria",10],["Manuel",8],["luis",10]])
+print(str(lista_calificaciones_nueva)+"\n")
+
+###########################################################################################
+print("###########################################################################################\n")
+'''
+    Tambien en las listas diccionarios podemos usar las funciones de eliminar y actualizar
+    "del" y "update"
+'''
+del lista_calificaciones_nueva["Maria"]
+print(str(lista_calificaciones_nueva)+"\n")
+# Actualizamos la calificacion de kevin y al mismo tiempo actualizamos la lista agregando otro elemento
+lista_calificaciones_nueva.update({"Kevin":10,"Emanuel":10})
+print(str(lista_calificaciones_nueva)+"\n")
---
+++ Python/app_16_interacion.py
@@ -0,0 +1,40 @@
+#limpiar pantalla antes de comenzar
+import os
+#comando para limpiar pantalla
+os.system ("cls") 
+
+calificaciones = {"Kevin":8,"Rodrigo":9,"Rodolfo":10,"Jesus":10,"Uriel":8}
+print("\tLista orginal sin modificaciones\n"+str(calificaciones)+"\n")
+
+'''
+    Para mostrar los datos de una lista no hace falta realizar ningun ciclo, pero estos mismos pueden llegar
+    a requerirse cuando queremos ordenar los datos, modificar los datos cuando la lista sea grande, etc.
+    La forma para recorrerlos seria asi:
+'''
+# Este ciclo solo nos imprime las llavaes de los elementos o items de la lista
+for x in calificaciones:
+    print(x)
+
+# De esta manera nos puede imprir el contenido de los elemetos sin las llaves
+for x in calificaciones:
+    print(calificaciones[x])
+
+print("\n*********** ********** ************ ************** *********\n")
+# Pero las formas anteriores son incorrectas ya que pueden representar un potencial peligro al momento de
+# utilizarlas en un proyecto de grande, ya que la impresion no esta condicionada y podria confundir, por lo
+# que se utilizan los siguientes metodos para condicionar lo que nosotros queremos que se nos muestre en
+# pantalla. Por ejemplo, yo mostrare primero todas la llaves, luego el contenido de los elementos y por 
+# ultimo todo el item o elemento de la lista, incluyendo su correspondiente llave.
+
+for key in calificaciones.keys():
+    # la variable key almacena solo las llaves de la lista
+    print(key)
+print("\n")
+for value in calificaciones.values():
+    # la variable value almacena los valores de cada item, sin su llave
+    print(value)
+print("\n")
+for item in calificaciones.items():
+    # la variable item almacena todo todo el elemento de la lista, incluyendo su llave
+    print(item)
+print("\n")
